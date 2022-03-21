bc7214f8b6bbb97f810867620ace08f360c6d723
Zehiael Ramos <zramos@eiimt.com>
2022-03-21 13:36:36 -0600

    Change item persons for players

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,26 @@ use std::collections::HashMap;
 
 fn main() -> Result<(), Error> {
 
-	let mut persons = HashMap::new();
+	let mut players = HashMap::new();
 
-	persons.insert(0,"Zehiael Ramos".to_string());
-	persons.insert(1,"Brenda Casillas".to_string());
-	persons.insert(2,"Daniel Rocha".to_string());
-	persons.insert(3,"Grace Almanza".to_string());
-	persons.insert(4,"Monica Rivera".to_string());
-	persons.insert(5,"Dante Robles".to_string());
-	persons.insert(6,"Oscar Amilkar".to_string());
-	persons.insert(7,"Javier Guerra".to_string());
-	persons.insert(8,"Andres Gomez".to_string());
-	persons.insert(9,"Jesus Yepes".to_string());
-	persons.insert(10,"Javier Guerra".to_string());
-	persons.insert(11,"Manuel Uribe".to_string());
-	persons.insert(12,"Ivan Venegas".to_string());
-	persons.insert(13,"Irving Eiimt".to_string());
-	persons.insert(14,"Jenry Lievano".to_string());
-	persons.insert(15,"Norberto Ruiz".to_string());
-	persons.insert(16,"Ruben Bautista".to_string());
-	persons.insert(17,"Maurizio Patino".to_string());
+	players.insert(0,"Zehiael Ramos".to_string());
+	players.insert(1,"Brenda Casillas".to_string());
+	players.insert(2,"Daniel Rocha".to_string());
+	players.insert(3,"Grace Almanza".to_string());
+	players.insert(4,"Monica Rivera".to_string());
+	players.insert(5,"Dante Robles".to_string());
+	players.insert(6,"Oscar Amilkar".to_string());
+	players.insert(7,"Javier Guerra".to_string());
+	players.insert(8,"Andres Gomez".to_string());
+	players.insert(9,"Jesus Yepes".to_string());
+	players.insert(10,"Javier Guerra".to_string());
+	players.insert(11,"Manuel Uribe".to_string());
+	players.insert(12,"Ivan Venegas".to_string());
+	players.insert(13,"Irving Eiimt".to_string());
+	players.insert(14,"Jenry Lievano".to_string());
+	players.insert(15,"Norberto Ruiz".to_string());
+	players.insert(16,"Ruben Bautista".to_string());
+	players.insert(17,"Maurizio Patino".to_string());
 
     let mut rng = rand::thread_rng();
 
@@ -40,7 +40,7 @@ fn main() -> Result<(), Error> {
 
 		let id: u8 = rng.gen_range(0..17);
 
-    	println!("{:} {:?}", _msg, persons.get(&id));
+    	println!("{:} {:?}", _msg, players.get(&id));
 	}
 
     Ok(())
